f913827c973a99ad0187775f4c4499e3baab5a19
baojingyinyin <yuyin22@gmail.com>
2020-12-02 21:41:02 -0800

    test function drawing and mouseDragged

--- demo/client/js/script.js
+++ demo/client/js/script.js
@@ -47,12 +47,37 @@
             console.log('🌍 Connected to server');
 
             // Create a Whiteboard instance
-            const whiteboard = new Whiteboard(canvas, socket, '#3c72de');
+            
+//             const whiteboard = new Whiteboard(canvas, socket, '#3c72de');
+            
+            const whiteboard = new Drawing ();
 
             // Expose the whiteboard instance
             window.whiteboard = whiteboard;
 
             printDemoMessage();
         });
+        
+//         test test test
+        
+        function Drawing(data){
+            noStroke(); 
+             fill(50,200,random(150,255),50);
+             ellipse(data.x,data.y, random (10,30));
+        }
+        function mouseDragged (){
+            
+            var data = {
+             x: mouseX,
+             y: mouseY
+                 }
+   
+        socket.emit ('mouse', data);
+             noStroke(); 
+             fill(255,0,random(50,150),50);
+             ellipse(mouseX,mouseY, random (10,30));
+                                }
+        
+        
     });
 })(io, Whiteboard);
